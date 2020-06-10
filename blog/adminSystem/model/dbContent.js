84509ae8d17d34e27807d96927bb7e1bcaee17ab
TAC-5 <3467913546@qq.com>
2020-06-10 22:31:04 +0800

    博客项目

--- blog/adminSystem/model/dbContent.js
+++ blog/adminSystem/model/dbContent.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // 数据库连接
-mongoose.connect('mongodb://localhost:27017/db_blog', {
+mongoose.connect('', {
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => {
     console.log("数据库连接成功");
